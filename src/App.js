30930dbf17db21164a8dbf506b85e7675c76cb30
Durrell Jules <dujules23@gmail.com>
2021-06-16 11:08:46 -0400

    Created handleAdd ecent handler and applied it to each number button, connected successfully

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import CalcButton from './components/CalcButton';
 
 import reducer, { initialState } from './reducers/index'
 
+import { addOne, applyNumber } from './actions/index'
+
 
 function App() {
-
+  // Imported useReducer hook to get access to state
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  console.log(state)
+  // console.log(state)
 
+  const handleAdd = (number) =>{
+    dispatch(applyNumber(number))
+
+  }
+  
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -24,10 +31,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -37,21 +44,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleAdd(1)}/>
+              <CalcButton value={2} onClick={() => handleAdd(2)}/>
+              <CalcButton value={3} onClick={() => handleAdd(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleAdd(4)}/>
+              <CalcButton value={5} onClick={() => handleAdd(5)}/>
+              <CalcButton value={6} onClick={() => handleAdd(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleAdd(7)}/>
+              <CalcButton value={8} onClick={() => handleAdd(8)}/>
+              <CalcButton value={9} onClick={() => handleAdd(9)}/>
             </div>
 
             <div className="row">
